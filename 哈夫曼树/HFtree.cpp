2d7204b859158d23b331f56413775ecaaee7647e
jiangbing22 <114992057+jiangbing22@users.noreply.github.com>
2023-11-22 11:49:02 +0800

    11.22

--- 哈夫曼树/HFtree.cpp
+++ 哈夫曼树/HFtree.cpp
@@ -1,1 +1,60 @@
 #include "HFtree.h"
+
+void HFtree::createtree(int n, char* characters, int* weights)
+{
+	priority_queue<HuffmanNode*, vector<HuffmanNode*>, CompareNodes> minHeap;
+	for(int i =0;i<n;i++)
+	{
+		HuffmanNode* node = new HuffmanNode(characters[i], weights[i]);
+		minHeap.push(node);
+	}
+	root = buildtree(minHeap);
+
+
+}
+
+void HFtree::savetree(HuffmanNode* node,const char* outfile) 
+{
+
+}
+
+void HFtree::destorytree(HuffmanNode* node)
+{
+	if (node != nullptr)
+	{
+		destorytree(node->left);
+		destorytree(node->right);
+		delete node;
+	}
+}
+
+HuffmanNode* HFtree::buildtree(priority_queue<HuffmanNode*, vector<HuffmanNode*>, CompareNodes>& minHeap)
+{
+	while (minHeap.size() > 1)
+	{
+		HuffmanNode* left = minHeap.top();
+		minHeap.pop();
+
+		HuffmanNode* right = minHeap.top();
+		minHeap.pop();
+		
+		HuffmanNode* internalNode = new HuffmanNode('$', left->weight + right->weight);
+		internalNode->left = left;
+		internalNode->right = right;
+
+		minHeap.push(internalNode);
+	}
+	return minHeap.top();
+}
+
+void HFtree::encode(HuffmanNode* node, string code, map<char, string>& codes)
+{
+	if (node != nullptr) {
+		if (node->data != '$') {
+			codes[node->data] = code;
+		}
+
+		encode(node->left, code + "0", codes);
+		encode(node->right, code + "1", codes);
+	}
+}
--- 哈夫曼树/HFtree.h
+++ 哈夫曼树/HFtree.h
@@ -1,18 +1,36 @@
-#pragma once
-class HFtree
-{
-protected:
-	class HFnode
-	{
-	public:
-		int weight;
-		HFnode* lchild;
-		HFnode* rchild;
-		HFnode(int w = 0, HFnode* l = nullptr, HFnode* r = nullptr) :weight(w), lchild(l), rchild(r) {};
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <queue>
+#include <map>
+using namespace std;
+struct HuffmanNode {
+    char data;
+    int weight;
+    HuffmanNode* left;
+    HuffmanNode* right;
 
-	};
-	HFnode* root;
-public:
-
+    HuffmanNode(char d, int w) : data(d), weight(w), left(nullptr), right(nullptr) {}
 };
 
+// 优先队列比较器，用于构建最小堆
+struct CompareNodes {
+    bool operator()(HuffmanNode* lhs, HuffmanNode* rhs) const {
+        return lhs->weight > rhs->weight;
+    }
+};
+class HFtree{
+public:
+    HFtree() :root(nullptr) {};
+    ~HFtree(){destorytree(root);}
+    void createtree(int n, char* characters, int* weights);
+    void savetree(HuffmanNode* node,const char* outfile);
+    
+    
+
+private:
+    HuffmanNode* root;
+    void destorytree(HuffmanNode* node);
+    HuffmanNode* buildtree(priority_queue<HuffmanNode*, vector<HuffmanNode*>, CompareNodes>& minHeap);
+    void encode(HuffmanNode* node, string code, map<char, string>& codes);
+};
